33e75502412371874fe5eadc48a1a6330b76e2d0
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-29 14:58:17 +0300

    created registration route and template and implemented flask-login extention

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://derrick:daniel@localhost/qwerty'
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
 class ProdConfig(Config):
     pass
 
