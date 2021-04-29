8ebaa2308acc6129d08d105e192c55d2cec729f5
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-29 16:28:31 +0300

    imported updateprofile form

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,8 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..models import User
 from .. import db,photos
+from flask_login import login_required
+from .forms import UpdateProfile
 
 @main.route('/')
 def index():
